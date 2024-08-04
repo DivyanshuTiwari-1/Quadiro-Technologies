7468a6da2fbd5c05dd32303f89b63dfab0ea3e0b
Divyanshu Tiwari <128217067+DivyanshuTiwari-1@users.noreply.github.com>
2024-08-04 10:40:45 +0530

    Update route.ts

--- app/api/users/read/route.ts
+++ app/api/users/read/route.ts
@@ -18,7 +18,10 @@ import { NextRequest,NextResponse } from "next/server";
 export async function GET(request:NextRequest){
     try{
         const cars: CarDocument[] = await getCars();
-        
+         res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+      res.setHeader('Pragma', 'no-cache');
+      res.setHeader('Expires', '0');
+      res.setHeader('Surrogate-Control', 'no-store');
         return  NextResponse.json(
             cars
            )
